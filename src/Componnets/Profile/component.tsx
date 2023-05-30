4303978ddafc10514e57fb3d44629ea6af39073f
Minh Tâm <tam.minkshin@gmail.com>
2023-05-30 16:57:58 +0700

    set up tailwind

--- src/Componnets/Profile/component.tsx
+++ src/Componnets/Profile/component.tsx
@@ -1,18 +1,20 @@
-import * as React from 'react';
+import * as React from "react";
+import { Disclosure } from "@headlessui/react";
 
-interface ProfileProps {
-    
-}
- 
-interface ProfileState {
-    
-}
- 
+interface ProfileProps {}
+
+interface ProfileState {}
+
 class ProfileComponent extends React.Component<ProfileProps, ProfileState> {
-    state = {  }
-    render() { 
-        return <>Đây là ProfileComponent</>;
-    }
+  state = {};
+  render() {
+    return (
+      <Disclosure>
+        <Disclosure.Button className="py-2">Is team pricing available?</Disclosure.Button>
+        <Disclosure.Panel className="text-gray-500">Yes! You can purchase a license that you can share with your entire team.</Disclosure.Panel>
+      </Disclosure>
+    );
+  }
 }
- 
+
 export default ProfileComponent;
