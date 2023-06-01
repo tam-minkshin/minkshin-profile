1caf7e9ab04d02d0197761de61fb90f9df1ba371
Minh Tâm <tam.minkshin@gmail.com>
2023-06-01 17:57:28 +0700

    fix gap

--- src/Core/Grid/grid.core.tsx
+++ src/Core/Grid/grid.core.tsx
@@ -2,11 +2,21 @@ interface GridProps {
   item?: boolean;
   children: React.ReactNode;
   xs?: number;
-  gap?:number;
+  gap?: number;
 }
 const Grid = (props: GridProps) => {
-  const { item = false, children, xs = 6, gap=0 } = props;
-  return <div className={`${item ? `flex-auto ${xs === 12 ? "w-full" : `w-${xs}/>12`}` : `flex flex-wrap`}`} style={{columnGap:`${gap}rem`}}>{children}</div>;
+  const { item = false, children, xs = 6, gap = 1 } = props;
+  const handleClass = () => {
+    if (item) {
+      return `flex-auto ${xs === 12 ? "w-full" : `w-${xs}/>12`}`;
+    }
+    return `flex flex-row flex-wrap gap-${gap}`;
+  };
+  return (
+    <div className={handleClass()} >
+      {children}
+    </div>
+  );
 };
 
 export default Grid;
