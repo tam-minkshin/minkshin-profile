1b41ed6d892733c970c1edc809b6c177251eadee
Minh Tâm <tam.minkshin@gmail.com>
2023-06-02 16:18:05 +0700

    test config change theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,21 @@
+export const handleTheme = ()=>{
+  if(true){
+    return test;
+  }
+};
+test={
+  "light": "#F9F5F6",
+  "dark": "#0d1117",
+  "color-success": "#7ee787",
+  "color-error": "#f85149",
+  "text-color": "#f0f6fc",
+  outline: "#d2a8ff",
+};
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      colors: {
-        "light": "#F9F5F6",
-        "dark": "#0d1117",
-        "color-success": "#7ee787",
-        "color-error": "#f85149",
-        "text-color": "#f0f6fc",
-        outline: "#d2a8ff",
-      },
+      colors: handleTheme,
     },
   },
   plugins: [],
